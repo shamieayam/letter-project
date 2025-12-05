c52562cc094b29eccb98adf635c94003ea9d90a5
shamieayam <jjustchaa@gmail.com>
2025-12-05 20:06:24 +0700

    Update hehe

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `ASSALAMUALAIKUM WARAHMATULLAHI WABARAKATUH! kak, semangat terus ya, meskipun pastinya cape banget ngejalaninnya, U DO A GREAT JOB KAK!!!! PROUDOFU, wink. `;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
